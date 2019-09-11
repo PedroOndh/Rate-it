fcdceb1fd747a51e1ffbff7da65b437bd948a3d4
PedroOndh <pedro.ondiviela@gmail.com>
2019-09-11 16:23:24 +0200

    Small change in the AddCollection structure

--- src/AddCollection.js
+++ src/AddCollection.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
 class AddCollection extends React.Component {
-    state = { newcollection: '' }
+    state = {
+      newcollection: '' ,
+      showing: false
+    }
     render() {
+        let formClassName = "popup__addnew-form"
+        if (this.state.showing) {
+            formClassName += ' popup__addnew-form--visible'
+        }
         return (
             <div className="popup__addnew" >
                 <span className="popup__addnew-plus" onClick={this.showAddNewInput} >+</span>
-                <form className="popup__addnew-form"  onSubmit={event => event.preventDefault()}>
+                <form className={ formClassName }  onSubmit={event => event.preventDefault()}>
                     <input type="text" onChange={this.setNewCollectionName} className="popup__addnew-input" ></input>
                     <button className="popup__addnew-button"  onClick={() => this.props.addNewCollection(this.state.newcollection) } >ADD NEW COLLECTION</button>
                 </form>
@@ -14,7 +21,7 @@ class AddCollection extends React.Component {
         )
     }
     showAddNewInput = () => {
-        document.querySelector('.popup__addnew-form').classList.toggle('popup__addnew-form--visible');
+        this.setState({showing: !this.state.showing})
     }
     setNewCollectionName = (event) => {
         this.setState({ newcollection: event.target.value })
